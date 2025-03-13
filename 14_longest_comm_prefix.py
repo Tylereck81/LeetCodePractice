798bca32ae6bfe9ab0663146f7cb5c5b5f20318c
Tyler Eck <tylereck81@yahoo.com>
2025-03-13 02:19:19 +0200

    longest common prefix

---
+++ 14_longest_comm_prefix.py
@@ -0,0 +1,28 @@
+
+def longestCommonPrefix(strs: list[str]) ->str:
+    #length of shortest string
+    n = 10000
+    index = 0
+    for i in range(len(strs)): 
+        if len(strs[i])<n: 
+            n = len(strs[i]) 
+            index = i
+    flag = 1
+    c = -1
+
+    for i in range(n):
+        for j in range(len(strs)): 
+            if flag:
+                if strs[j][i] != strs[index][i]: 
+                    flag = 0
+        if flag == 0:
+            break
+        c = i
+        print(c)
+    
+    if c!=-1:
+        return strs[index][:c+1]
+    else: 
+        return []
+    
+print(longestCommonPrefix(["a"]))
