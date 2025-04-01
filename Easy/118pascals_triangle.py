22786706048f6ebf86f7ea62ce14ff371da060af
Tyler Eck <tylereck81@yahoo.com>
2025-04-01 10:17:43 -0600

    Other implementations of Pascal Triangle

--- Easy/118pascals_triangle.py
+++ Easy/118pascals_triangle.py
@@ -1,3 +1,5 @@
+# TIme complexity: O(n^2)
+# Pascal's Triangle
 def generate(numRows):
     if numRows == 1: 
         return [[1]]
@@ -14,6 +16,43 @@ def generate(numRows):
     
     return l
 
-print(generate(5))
-print(generate(4))
-print(generate(3))
+# Recursive Solution
+# Time complexity: O(n^2) 
+# Space complexity: O(n)
+def generate2(numRows: int): 
+    if numRows == 1: 
+        return [[1]]
+        
+    prevRows = generate(numRows -1)
+    prevRow = prevRows[-1]
+    newRow = [1]*numRows
+
+    for i in range(1,numRows-1): 
+        newRow[i]  = prevRow[i] + prevRow[i-1]
+    
+    prevRows.append(newRow)
+    return prevRows
+
+# COMBINATION OF BOTH OR DP SOLUTION 
+# TIme complexity: O(n^2)
+# Space complexity: O(n)
+def generate3(numRows: int): 
+    if numRows == 1: 
+        return [[1]]
+
+    prevRows = generate3(numRows-1)
+    prevRow = prevRows[-1]
+    newRow = [1]
+
+    for i in range(0,len(prevRow)-1): 
+        newRow.append(prevRow[i]+prevRow[i+1])
+    newRow.append(1)
+
+    prevRows.append(newRow)
+    return prevRows
+
+print(generate3(5))
+print(generate3(4))
+print(generate3(3))
+
+
