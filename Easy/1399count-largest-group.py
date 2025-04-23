dd1c1f803dc1a815f30e09056f80f428d6e6f851
Tyler Eck <tylereck81@yahoo.com>
2025-04-23 11:02:43 -0600

    Count largest group

---
+++ Easy/1399count-largest-group.py
@@ -0,0 +1,22 @@
+def countLargestGroup(n: int) -> int:
+    d= {}
+    largest_groups = 0
+    group_size = 0
+    for num in range(1, n+1):
+        c = 0
+        for i in range(len(str(num))): 
+            c+=int(str(num)[i])
+        if c in d: 
+            d[c] +=1 
+        else: 
+            d[c] = 1
+
+        if d[c] > group_size: 
+            group_size = d[c] 
+            largest_groups = 1
+        elif d[c] == group_size:
+            largest_groups+=1
+
+    return largest_groups
+
+print(countLargestGroup(1))
